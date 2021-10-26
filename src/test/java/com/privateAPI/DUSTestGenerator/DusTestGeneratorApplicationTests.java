469961513accaa60e80cb42847e53015301f3df0
Kristián Sokol <kikosokol@gmail.com>
2021-10-26 15:02:37 +0200

    initial commit

---
+++ src/test/java/com/privateAPI/DUSTestGenerator/DusTestGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.privateAPI.DUSTestGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DusTestGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
