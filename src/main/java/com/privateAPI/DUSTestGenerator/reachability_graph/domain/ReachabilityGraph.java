34094074c40e24e02863f1c2fbb9bdfb4e233838
Kristián Sokol <kikosokol@gmail.com>
2021-12-08 04:41:58 +0100

    - comment mongoDb dependency in pom file - delete id from ReachabilityGraph and delete all with mongoDb

--- src/main/java/com/privateAPI/DUSTestGenerator/reachability_graph/domain/ReachabilityGraph.java
+++ src/main/java/com/privateAPI/DUSTestGenerator/reachability_graph/domain/ReachabilityGraph.java
@@ -3,18 +3,14 @@ package com.privateAPI.DUSTestGenerator.reachability_graph.domain;
 import com.privateAPI.DUSTestGenerator.objects_for_graph_and_tree.domain.Edge;
 import com.privateAPI.DUSTestGenerator.objects_for_graph_and_tree.domain.Vertex;
 import lombok.Data;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
+
 
 import java.util.List;
 
-@Document("reachability_graph")
+
 @Data
 public class ReachabilityGraph
 {
-    @MongoId
-    private ObjectId id;
     private List<Vertex> vertices;
     private List<Edge> edges;
 
