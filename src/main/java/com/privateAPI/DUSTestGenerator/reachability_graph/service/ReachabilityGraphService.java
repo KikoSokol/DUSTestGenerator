9f5f031f05916c70687935d923a594e284a2c166
Kristián Sokol <kikosokol@gmail.com>
2022-04-28 16:35:57 +0200

    feat: pridal som funkciu do servisu pre graf dosiahnuteľnsoti na vytvorenie grafu dosiahnuteľnosti priamo z petriho siete

--- src/main/java/com/privateAPI/DUSTestGenerator/reachability_graph/service/ReachabilityGraphService.java
+++ src/main/java/com/privateAPI/DUSTestGenerator/reachability_graph/service/ReachabilityGraphService.java
@@ -1,5 +1,6 @@
 package com.privateAPI.DUSTestGenerator.reachability_graph.service;
 
+import com.privateAPI.DUSTestGenerator.petri_nets.dto.PetriNetDto;
 import com.privateAPI.DUSTestGenerator.reachability_graph.controller.request.ReachabilityGraphGeneratorRequest;
 import com.privateAPI.DUSTestGenerator.reachability_graph.dto.ReachabilityGraphDto;
 import com.privateAPI.DUSTestGenerator.reachability_graph.dto.ReachabilityGraphGeneratorResultDto;
@@ -8,5 +9,5 @@ import com.privateAPI.DUSTestGenerator.reachability_graph.dto.ReachabilityGraphR
 public interface ReachabilityGraphService
 {
     ReachabilityGraphGeneratorResultDto getReachabilityGraph(ReachabilityGraphGeneratorRequest reachabilityGraphGeneratorRequest);
-
+    ReachabilityGraphGeneratorResultDto fromPetriNetToReachabilityGraph(PetriNetDto petriNetDto);
 }
